c055199a7aebff2c97a943f74ec5c3b0abb51853
Jiwon Park <xoxojw.dev@gmail.com>
2023-06-28 10:28:43 +0900

    Feat: firebase 초기화 중에 '로딩 중' 문구 출력되도록 설정

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,9 @@ import app from "fbase"; // Firebase 앱 초기화 코드를 추가하세요.
 
 const App = () => {
   const dispatch = useDispatch();
+
+  // firebase 초기화 여부
+  const [init, setInit] = useState(false);
   const [userInfo, setUserInfo] = useState(null);
 
   useEffect(() => {
@@ -26,13 +29,15 @@ const App = () => {
         setUserInfo(null);
         dispatch(setIsLoggedIn(false));
       }
+      setInit(true);
     });
-  }, []);
+  }, [dispatch]);
 
   return (
     <>
       <GlobalStyle />
-      <Router isLoggedIn={Boolean(userInfo)} userInfo={userInfo} />
+      {/* {firebase가 초기화 되는데 시간이 걸려서, 초기화 되기 전이면 "로딩 중" 멘트 출력} */}
+      {init ? <Router isLoggedIn={Boolean(userInfo)} userInfo={userInfo} /> : "로딩 중.."}
     </>
   );
 };

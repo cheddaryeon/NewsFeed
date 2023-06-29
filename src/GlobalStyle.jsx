6ba20cf8f600c4581af4f21203439082ac0619cb
yui62yui <you@example.com>
2023-06-29 16:53:43 +0900

    Style: "추가, 구글폰트 추가(Noto Sans KR"

--- src/GlobalStyle.jsx
+++ src/GlobalStyle.jsx
@@ -4,6 +4,10 @@ import reset from "styled-reset"
 const GlobalStyle = createGlobalStyle`
   ${reset}
 
+  * {
+    font-family: 'Noto Sans KR', sans-serif;
+  }
+
   a {
     text-decoration: none;
     color: #000;
@@ -16,7 +20,9 @@ const GlobalStyle = createGlobalStyle`
   }
 
   body {
+    font-family: 'Noto Sans KR', sans-serif;
     font-size: 16px;
+
     margin: 0 auto;
     text-align: center;
 

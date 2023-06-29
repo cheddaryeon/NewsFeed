f20976024edd200c72e890c96921088ca449d6b7
AJ3504 <plzgimmeajob@gmail.com>
2023-06-29 10:35:15 +0900

    wednesday

--- src/components/comments/CommentsList.jsx
+++ src/components/comments/CommentsList.jsx
@@ -1,11 +1,70 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import AddComments from "./AddComments";
+import comments from "redux/modules/comments";
+import { useParams } from "react-router-dom";
 
 const CommentsList = () => {
+  //
+  const { contentsId } = useParams();
+  console.log("contentsId2 =>", contentsId);
+
+  //
+  const comments = useSelector((state) => state.comments);
+  console.log("state.comments의 값", comments);
+
+  const filteredComments = comments.filter((comment) => {
+    return comment.contentsId === contentsId;
+  });
+  console.log("흠", filteredComments);
+
+  return filteredComments.map((comment) => {
+    return (
+      <div key={comment?.contentsId}>
+        <p>결재내용: {comment.commentsBody}</p>
+        <button>수정</button>
+        <button>삭제</button>
+      </div>
+    );
+  });
+
   // 댓글 목록 컴포넌트
+  // const dispatch = useDispatch();
+  // const comments = useSelector((state) => state.comments);
 
-  return (
-    <div>CommentsList</div>
-  )
-}
+  // const [commentsWriter, setCommentsWriter] = useState("");
+  // const [commentsBody, setCommentsBody] = useState("");
 
-export default CommentsList
+  // useEffect(() => {}, [commentsWriter, commentsBody]);
+
+  // //아래 동작X
+  // const commentsMap = comments.map((comment) => {
+  //   return (
+  //     <div
+  //       style={{
+  //         border: "1px solid black",
+  //         padding: "10px",
+  //         margin: "10px",
+  //       }}
+  //       key={comment?.id}
+  //     >
+  //       <p>결재자 : {comment.commentsWriter}</p>
+  //       <p>결재내용 : {comment.commentsBody}</p>
+  //       <button>수정</button>
+  //       <button
+  //         onClick={() => {
+  //           dispatch({
+  //             type: "DELETE_COMMENT",
+  //             payload: comment.id,
+  //           });
+  //         }}
+  //       >
+  //         삭제
+  //       </button>
+  //     </div>
+  //   );
+  // });
+  // return <div>{commentsMap}</div>;
+};
+
+export default CommentsList;
--- src/shared/Router.js
+++ src/shared/Router.js
@@ -1,15 +1,20 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+//
 import Main from "pages/Main";
+import ContentsForm from "pages/ContentsForm";
 import Detail from "pages/Detail";
 import MyProfile from "pages/MyProfile";
+import Header from "components/ui/Header";
 
 const Router = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Main />} />
-        <Route path="/:contentsId" element={<Detail />} />
-        <Route path="/:userId" element={<MyProfile />} />
+        <Route path="/contentsForm" element={<ContentsForm />} />
+        <Route path="/detail/:contentsId" element={<Detail />} />
+        {/* <Route path="/:UserId" element={<MyProfile />} /> */}
       </Routes>
     </BrowserRouter>
   );

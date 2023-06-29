01d2e05dd85a4691f4e418f7daea0748dffeaa9f
yui62yui <you@example.com>
2023-06-29 16:37:56 +0900

    Style: "추가, Header,GlobalStyle 스타일 추가'

--- src/GlobalStyle.jsx
+++ src/GlobalStyle.jsx
@@ -2,7 +2,26 @@ import { createGlobalStyle } from 'styled-components';
 import reset from "styled-reset"
 
 const GlobalStyle = createGlobalStyle`
-  ${reset}  
+  ${reset}
+
+  a {
+    text-decoration: none;
+    color: #000;
+  }
+
+  button {
+    cursor: pointer;
+    border: none;
+    background-color: #fff;
+  }
+
+  body {
+    font-size: 16px;
+    margin: 0 auto;
+    text-align: center;
+
+    color: #333
+  }
 `
 
 export default GlobalStyle;

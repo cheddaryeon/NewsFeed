d322c6efe54fde758bac59d7e82cb4c9777dd729
Jiwon Park <xoxojw.dev@gmail.com>
2023-06-30 23:18:26 +0900

    Feat: Google 사용자의 경우 비밀번호 변경탭 안보이게 숨김처리

--- src/components/profile/ChangeProfile.jsx
+++ src/components/profile/ChangeProfile.jsx
@@ -13,7 +13,7 @@ const ChangeProfile = () => {
     newPw: "",
     newPwCheck: "",
     newUserPic: "",
-  })
+  });
   const [pwError, setPwError] = useState(false);
   const [pwCheckTxt, setPwCheckTxt] = useState("");
   const [imgFileUrl, setImgFileUrl] = useState(currentUser.userPic);
@@ -47,7 +47,6 @@ const ChangeProfile = () => {
     }
   };
   
-
   const onImgFileChange = (e) => {
     const files = e.target?.files;
   
@@ -66,41 +65,40 @@ const ChangeProfile = () => {
     }
 
     // 이미지 firebase storage에 업데이트
-  };  
+  };
 
   const onClearImgFile = () => setImgFileUrl(currentUser.userPic);
 
-    // 프로필 사진 변경
+  // 프로필 사진 변경
   const handleChangeUserPic = async (e) => {
     e.preventDefault();
-  }
+  };
 
   // 닉네임 변경
-const handleChangeUserName = async (e) => {
-  e.preventDefault();
-  const { newUserName } = inputs;
-  if (newUserName !== currentUser.userName) {
-    const ok = window.confirm(`닉네임을 '${newUserName}'으로 변경하시겠어요?`);
-    if (ok) {
-      try {
-        await updateProfile(authService.currentUser, { displayName: newUserName });
-        dispatch(
-          setUserInfo({
-            ...currentUser,
-            userName: newUserName,
-          })
-        );
-        window.alert("닉네임이 정상적으로 변경되었습니다.");
-      } catch (error) {
-        window.alert(error);
-        console.log(error);
+  const handleChangeUserName = async (e) => {
+    e.preventDefault();
+    const { newUserName } = inputs;
+    if (newUserName !== currentUser.userName) {
+      const ok = window.confirm(`닉네임을 '${newUserName}'으로 변경하시겠어요?`);
+      if (ok) {
+        try {
+          await updateProfile(authService.currentUser, { displayName: newUserName });
+          dispatch(
+            setUserInfo({
+              ...currentUser,
+              userName: newUserName,
+            })
+          );
+          window.alert("닉네임이 정상적으로 변경되었습니다.");
+        } catch (error) {
+          window.alert(error);
+          console.log(error);
+        }
       }
+    } else {
+      alert("닉네임 변경사항이 없습니다.");
     }
-  } else {
-    alert("닉네임 변경사항이 없습니다.");
-  }
-};
-
+  };
 
   // 비밀번호 변경
   const handleChangePw = async (e) => {
@@ -120,11 +118,9 @@ const handleChangeUserName = async (e) => {
   return (
     <>
       <h2>My Profile</h2>
-        <UserPicPreview src={imgFileUrl} width="150px" height="150px" />
+      <UserPicPreview src={imgFileUrl} width="150px" height="150px" />
       <form onSubmit={handleChangeUserPic}>
-        <label htmlFor="inUserPic">
-          프로필 사진 변경
-        </label>
+        <label htmlFor="inUserPic">프로필 사진 변경</label>
         <input
           name="userPic"
           id="inUserPic"
@@ -135,21 +131,17 @@ const handleChangeUserName = async (e) => {
         <button onClick={onClearImgFile}>✖️</button>
       </form>
       <br />
-      <label htmlFor="inUserEmail">
-        가입 이메일 주소
-      </label>
-        <input
-          name="email"
-          id="inUserEmail"
-          type="text"
-          placeholder={currentUser.userEmail ? currentUser.userEmail : "Google 사용자"}
-          disabled
-        />
+      <label htmlFor="inUserEmail">가입 이메일 주소</label>
+      <input
+        name="email"
+        id="inUserEmail"
+        type="text"
+        placeholder={currentUser.userEmail ? currentUser.userEmail : "Google 사용자"}
+        disabled
+      />
       <br />
       <form onSubmit={handleChangeUserName}>
-        <label htmlFor="inUserName">
-          닉네임
-        </label>
+        <label htmlFor="inUserName">닉네임</label>
         <input
           name="newUserName"
           id="inUserName"
@@ -161,38 +153,39 @@ const handleChangeUserName = async (e) => {
         <input type="submit" value={"닉네임 변경"} />
         <br />
       </form>
-      <form onSubmit={handleChangePw}>
-        <label htmlFor="inNewPw">
-          비밀번호 변경
-        </label>
-        <input
-          name="newPw"
-          id="inNewPw"
-          type="password"
-          placeholder={currentUser.userEmail ? "변경할 비밀번호" : "Google 사용자는 비밀번호 설정 불가"}
-          value={inputs.newPw}
-          onChange={onChange}
-          disabled={!currentUser.userEmail}
-        />
-        <br />
-        <label htmlFor="inNewPwCheck">
-          비밀번호 변경 확인
-        </label>
-        <input
-          name="newPwCheck"
-          id="inNewPwCheck"
-          type="password"
-          placeholder={currentUser.userEmail ? "변경할 비밀번호 확인" : ""}
-          value={inputs.newPwCheck}
-          onChange={onChange}
-          disabled={!currentUser.userEmail}
-        />
-        {currentUser.userEmail ? <input type="submit" value={"비밀번호 변경"} /> : ""}
-      </form>
-      {inputs.newPwCheck && <p>{pwCheckTxt}</p>}
-      <br />
+      {authService.currentUser.providerData.some(
+        (userInfo) => userInfo.providerId === "google.com"
+      ) ? (
+        ""
+      ) : (
+        <>
+          <form onSubmit={handleChangePw}>
+            <label htmlFor="inNewPw">비밀번호 변경</label>
+            <input
+              name="newPw"
+              id="inNewPw"
+              type="password"
+              placeholder="변경할 비밀번호 입력"
+              value={inputs.newPw}
+              onChange={onChange}
+            />
+            <br />
+            <label htmlFor="inNewPwCheck">비밀번호 변경 확인</label>
+            <input
+              name="newPwCheck"
+              id="inNewPwCheck"
+              type="password"
+              placeholder="변경할 비밀번호 확인"
+              value={inputs.newPwCheck}
+              onChange={onChange}
+            />
+            <input type="submit" value={"비밀번호 변경"} />
+          </form>
+          {inputs.newPwCheck && <span>{pwCheckTxt}</span>}
+        </>
+      )}
     </>
-  )
+  );  
 }
 
 export default ChangeProfile;
